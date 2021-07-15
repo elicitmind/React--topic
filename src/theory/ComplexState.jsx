c5702c0b186260466ea163cc2e48c22f90d28f1e
elicitmind <elicitmind@gmail.com>
2021-07-15 08:25:19 +0200

    spread operator

--- src/theory/ComplexState.jsx
+++ src/theory/ComplexState.jsx
@@ -9,27 +9,35 @@ function App() {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
+    // setContact((prevValue) => {
+    //   if (name === "fName") {
+    //     return {
+    //       fName: value,
+    //       lName: prevValue.lName,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "lName") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: value,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "email") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: prevValue.lName,
+    //       email: value
+    //     };
+    //   }
+    // });
+
+    //EQUALS TO:
+    setContact((prevValue)=>{
+      return {
+        ...prevValue,
+        [name]: value
       }
-    });
+    })
   }
 
   return (
